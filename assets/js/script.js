e187bf01bb447c7714c06d8088798645dd2093a6
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-06-21 17:56:48 +0000

    feat: :sparkles: prevent division operandi from matching, create function to perform clean division
    If the operandi match, the division was too easy. That is why the change was made.

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,23 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "division") {
+        /** Perform modulos on greater number and remove result in order to allow for clean division.*/ 
+        function cleanDivide() {
+            if (num1 !== num2) {
+                if (num1 > num2) {
+                    num1 -= num1 % num2;
+                } else {
+                    num2 -= num2 % num1;
+                }
+            }
+        }
+
+        cleanDivide();
+        // Prevent operandi from matching.
+        while (num1 === num2) {
+            num1 = Math.ceil(Math.random()*25);
+            cleanDivide();
+        }
         displayDivisionQuestion(num1, num2);
     }
     else {
@@ -127,12 +144,6 @@ function displayMultiplyQuestion(operand1, operand2) {
 };
 
 function displayDivisionQuestion(operand1, operand2) {
-    // Perform modulos on greater operandi and remove result in order to allow for clean division.
-    if (operand1 > operand2) {
-        operand1 -= operand1 % operand2;
-    } else {
-        operand2 -= operand2 % operand1;
-    }
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 < operand2 ? operand1 : operand2;
     document.getElementById('operator').textContent = "/";
